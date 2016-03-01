f335b6be251fb90c9140f015fedf7ad9dea9e488
Memnarch <Sebal007@freenet.de>
2016-03-01 20:14:24 +0100

    placed all normal and coinblocks

--- Level_1_1.Entities.pas
+++ Level_1_1.Entities.pas
@@ -43,7 +43,7 @@ const
   CBottom = 0;
 
 var
-  GEntity: array[0..16] of TEntity =
+  GEntity: array[0..70] of TEntity =
   (
     (
       Active: 1;
@@ -131,8 +131,324 @@ var
       X: 360;
       Y: 96;
       {$i CoinBlock.ent}
-    )
+    ),
     //Endof first Blockgroup
+    //Second Blockgroup
+    //   ########   ###?
+    //
+    //
+    //
+    //#?#              #
+    //
+    //
+    //
+    //////////////////////////
+    (
+      Active: 1;
+      X: 1240;
+      Y: 160;
+      {$i Block.ent}
+    ),
+    (
+      Active: 1;
+      X: 1256;
+      Y: 160;
+      {$i ItemBlockEmpty.ent}
+    ),
+    (
+      Active: 1;
+      X: 1256;
+      Y: 160;
+      {$i CoinBlock.ent}
+    ),
+    (
+      Active: 1;
+      X: 1272;
+      Y: 160;
+      {$i Block.ent}
+    ),
+    (
+      Active: 1;
+      X: 1288;
+      Y: 96;
+      {$i Block.ent}
+    ),
+    //
+    (
+      Active: 1;
+      X: 1304;
+      Y: 96;
+      {$i Block.ent}
+    ),
+    (
+      Active: 1;
+      X: 1320;
+      Y: 96;
+      {$i Block.ent}
+    ),
+    (
+      Active: 1;
+      X: 1336;
+      Y: 96;
+      {$i Block.ent}
+    ),
+    (
+      Active: 1;
+      X: 1352;
+      Y: 96;
+      {$i Block.ent}
+    ),
+    (
+      Active: 1;
+      X: 1368;
+      Y: 96;
+      {$i Block.ent}
+    ),
+    (
+      Active: 1;
+      X: 1384;
+      Y: 96;
+      {$i Block.ent}
+    ),
+    (
+      Active: 1;
+      X: 1400;
+      Y: 96;
+      {$i Block.ent}
+    ),
+    //
+    (
+      Active: 1;
+      X: 1464;
+      Y: 96;
+      {$i Block.ent}
+    ),
+    (
+      Active: 1;
+      X: 1480;
+      Y: 96;
+      {$i Block.ent}
+    ),
+    (
+      Active: 1;
+      X: 1496;
+      Y: 96;
+      {$i Block.ent}
+    ),
+    (
+      Active: 1;
+      X: 1512;
+      Y: 96;
+      {$i ItemBlockEmpty.ent}
+    ),
+    (
+      Active: 1;
+      X: 1512;
+      Y: 96;
+      {$i CoinBlock.ent}
+    ),
+    (
+      Active: 1;
+      X: 1512;
+      Y: 160;
+      {$i Block.ent}
+    ),
+    //end of second BlockGroup
+    //third Blockgroud
+    //         ?
+    //
+    //
+    //
+    //##    ?  ?  ?
+    //
+    //
+    //
+    //////////////////////////
+    (
+      Active: 1;
+      X: 1608;
+      Y: 160;
+      {$i Block.ent}
+    ),
+    (
+      Active: 1;
+      X: 1624;
+      Y: 160;
+      {$i Block.ent}
+    ),
+    (
+      Active: 1;
+      X: 1704;
+      Y: 160;
+      {$i ItemBlockEmpty.ent}
+    ),
+    (
+      Active: 1;
+      X: 1704;
+      Y: 160;
+      {$i CoinBlock.ent}
+    ),
+    (
+      Active: 1;
+      X: 1752;
+      Y: 160;
+      {$i ItemBlockEmpty.ent}
+    ),
+    (
+      Active: 1;
+      X: 1752;
+      Y: 160;
+      {$i CoinBlock.ent}
+    ),
+    (
+      Active: 1;
+      X: 1752;
+      Y: 96;
+      {$i ItemBlockEmpty.ent}
+    ),
+    (
+      Active: 1;
+      X: 1752;
+      Y: 96;
+      {$i CoinBlock.ent}
+    ),
+    (
+      Active: 1;
+      X: 1800;
+      Y: 160;
+      {$i ItemBlockEmpty.ent}
+    ),
+    (
+      Active: 1;
+      X: 1800;
+      Y: 160;
+      {$i CoinBlock.ent}
+    ),
+    //end of third blockgroup
+    //fourth blockgroup
+    //   ###    #??#
+    //
+    //
+    //
+    //#          ##
+    //
+    //
+    //
+    //////////////////////////
+    (
+      Active: 1;
+      X: 1896;
+      Y: 160;
+      {$i Block.ent}
+    ),
+    (
+      Active: 1;
+      X: 1944;
+      Y: 96;
+      {$i Block.ent}
+    ),
+    (
+      Active: 1;
+      X: 1960;
+      Y: 96;
+      {$i Block.ent}
+    ),
+    (
+      Active: 1;
+      X: 1976;
+      Y: 96;
+      {$i Block.ent}
+    ),
+    (
+      Active: 1;
+      X: 2056;
+      Y: 96;
+      {$i Block.ent}
+    ),
+    (
+      Active: 1;
+      X: 2072;
+      Y: 96;
+      {$i ItemBlockEmpty.ent}
+    ),
+    (
+      Active: 1;
+      X: 2072;
+      Y: 96;
+      {$i CoinBlock.ent}
+    ),
+    (
+      Active: 1;
+      X: 2088;
+      Y: 96;
+      {$i ItemBlockEmpty.ent}
+    ),
+    (
+      Active: 1;
+      X: 2088;
+      Y: 96;
+      {$i CoinBlock.ent}
+    ),
+    (
+      Active: 1;
+      X: 2072;
+      Y: 160;
+      {$i Block.ent}
+    ),
+    (
+      Active: 1;
+      X: 2088;
+      Y: 160;
+      {$i Block.ent}
+    ),
+    (
+      Active: 1;
+      X: 2104;
+      Y: 96;
+      {$i Block.ent}
+    ),
+    //end of fourth blockgroup
+    //fifth blockgroup
+    //
+    //
+    //
+    //
+    //##?#
+    //
+    //
+    //
+    //////////////////////////
+    (
+      Active: 1;
+      X: 2696;
+      Y: 160;
+      {$i Block.ent}
+    ),
+    (
+      Active: 1;
+      X: 2712;
+      Y: 160;
+      {$i Block.ent}
+    ),
+    (
+      Active: 1;
+      X: 2728;
+      Y: 160;
+      {$i ItemBlockEmpty.ent}
+    ),
+    (
+      Active: 1;
+      X: 2728;
+      Y: 160;
+      {$i CoinBlock.ent}
+    ),
+    (
+      Active: 1;
+      X: 2744;
+      Y: 160;
+      {$i Block.ent}
+    )
+    //end of fifth blockgroup
   );
 
 implementation
